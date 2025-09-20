ada8b7873be8006e8094f4be37cf56357605331e
daniyalhaider236 <hdaniyal189@gmail.com>
2025-09-20 19:02:27 +0000

    Comparison

---
+++ basics/ComparisonDatatypes.js
@@ -0,0 +1,44 @@
+console.log("Daniyal Haider")
+
+// ***************************** COMPARISONS**********************************
+
+// console.log(1 < 2)
+
+// console.log(1 > 2)
+
+// console.log(1 <= 2)
+
+// console.log(1 == 2)
+
+// console.log(1 != 2)
+
+// console.log(null < 0)
+
+// console.log(null > 0)
+
+// console.log(null == 0)
+
+// console.log(null <= 0)  // comparisons convert null into a number treating it as zero. therefore this is true.
+
+
+
+/*
+
+While doing comparisons, always try to compare the two values with the 
+same datatypes to avoid confusions
+
+*/
+
+
+// Strict check (===) {checks both value and datatype}
+
+console.log(23 === 23);
+console.log(23 === 46);
+console.log("dani" === 23)
+
+
+
+
+
+
+
