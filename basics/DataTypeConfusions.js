1539e05d98e46bc24931e7c93a8eaeabbd193dde
daniyalhaider236 <hdaniyal189@gmail.com>
2025-09-20 13:35:49 +0000

    DataType Confusions

---
+++ basics/DataTypeConfusions.js
@@ -0,0 +1,56 @@
+//console.log("Hey man we back here");
+
+let score = "33"
+let score2 = "33qvdj"
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber )
+
+let valueNumber = Number(score2)
+
+//console.log(typeof valueNumber)
+
+//console.log(valueNumber)
+
+let name = 123
+
+valueString = String(name)
+
+//console.log(typeof valueString)
+
+//console.log(valueString)
+
+
+let score3 = false 
+
+let booleanFind= Number(score3)
+
+//console.log(typeof booleanFind)
+//console.log(booleanFind)
+
+let isLoggedIn = 1
+booleanValue = Boolean(isLoggedIn);
+console.log(typeof booleanValue);
+console.log(booleanValue);
+
+
+
+
+
+// "33" >= 33
+// "33abc" >= NaN
+//  true >= 1   false >= 0
+
+
+
+
+
+
+
+
+
+
+
