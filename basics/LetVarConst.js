ef70534975267a6abbb761d3a2d836f183eca25d
daniyalhaider236 <hdaniyal189@gmail.com>
2025-09-20 13:51:25 +0000

    let var and const

---
+++ basics/LetVarConst.js
@@ -0,0 +1,21 @@
+
+const id = 245
+let name = "Daniyal"
+let password = "34532"
+ city = "samundri"
+city = "faisalabad"
+
+
+console.log(id)
+console.log(name)
+console.log(password)
+console.log(city)
+
+console.table([id,name,password,city])
+
+
+
+
+
+// dont use var because of issue in block scope and functional scope  
+
