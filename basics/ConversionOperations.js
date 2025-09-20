e68228f2dcd890f3813b98ca5c6f28857ed12314
daniyalhaider236 <hdaniyal189@gmail.com>
2025-09-20 18:43:26 +0000

    Conversion

---
+++ basics/ConversionOperations.js
@@ -0,0 +1,58 @@
+//console.log("we doing operations now");
+
+//*****************************  OPERATIONS *************************************
+let value = 3
+
+let negValue = -(value)
+
+console.log(negValue);
+
+// console.log(2+2);
+
+// console.log(2-2);
+
+// console.log(2*2);
+
+// console.log(2%2);
+
+// console.log(2**2);
+
+let str1 = "Daniyal"
+
+let str2 = " Haider"
+
+let str3 =  str1 + str2
+
+//console.log(str3);
+
+console.log("1" + 2 + 3);
+console.log(2 + 3 + "1");  
+
+/*
+
+if a string is written first ahead of two numbers, the whole operation would be 
+taken as a string.  if string is written at last, than the operands behind it would 
+be arithmetically solved, and the answer would be joined by the string
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
