0ae9a25dcb14825f848485bd3b7d07ef4cd5f3b8
daniyalhaider236 <hdaniyal189@gmail.com>
2025-09-20 20:32:53 +0000

    summary

---
+++ basics/dataTypesSummary.js
@@ -0,0 +1,48 @@
+console.log("here is the datatypes summary");
+
+// ******************************Primitive DataTypes ****************************************
+
+// String, number, Boolean , Null , undefined, BigInt, symbol
+
+// ************************** REFERENCE (Non-Primitive) type ********************************
+
+// Array, Objects, Functions
+
+// ******************************** Symbols **************************************
+
+const id = Symbol('4264')
+const AnotherId = Symbol('4264')
+console.log(id === AnotherId);
+
+/* 
+
+Symbol keyword give the variables uniqueness, i.e if they have same value, both are unique.
+
+
+*/
+
+// *****************************************Arrays****************************************
+
+let playingXi = ["virat","Rohit","hardik"]
+
+console.log(playingXi);
+ 
+// *****************************************Objects***************************************
+
+let product = {
+
+    name :"XIAOMI pro",
+    price: 45000,
+    warranty:"2 years"
+
+
+}
+
+console.log(product);
+
+
+
+
+
+
+
